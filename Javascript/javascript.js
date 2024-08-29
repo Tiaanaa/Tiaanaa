f8b8d6bd16d7325a90a3f71d49dda2e7dfdf9663
Tiaanaa <christianaomonola99@gmail.com>
2024-08-29 20:16:44 +0100

    javascript display method added

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -5,9 +5,17 @@ console.log(carName) */
 /* let firstName = "Christiana", lastName="Omonola", age=26;
 alert("Welcome " + firstName +" " + lastName+ " " + age) */
 
-function myFunction(){
+/* function myFunction(){
     document.getElementById("demo").innerHTML="Hello Javascript"
 }
 function image(){
     document.getElementById("myImage").src="/images/IMG-20200226-WA0005.jpg"
-}
+} */
+
+    function myFunction(){
+        /* document.getElementById("demo").innerHTML="Hello Javascript" */
+        /* document.write(59) */
+        /* window.alert(87) */
+        /* console.log(57) */
+        window.print()
+    }
