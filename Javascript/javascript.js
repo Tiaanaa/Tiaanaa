9b462940447a05794eb9546a7818e4f3fb2b1af0
Tiaanaa <christianaomonola99@gmail.com>
2024-08-30 22:41:29 +0100

    variables studied

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,7 +1,7 @@
 /* var carName = 'Volvo';
 alert(carName)
-console.log(carName) */
-
+console.log(carName)
+ */
 /* let firstName = "Christiana", lastName="Omonola", age=26;
 alert("Welcome " + firstName +" " + lastName+ " " + age) */
 
@@ -12,10 +12,14 @@ function image(){
     document.getElementById("myImage").src="/images/IMG-20200226-WA0005.jpg"
 } */
 
-    function myFunction(){
-        /* document.getElementById("demo").innerHTML="Hello Javascript" */
-        /* document.write(59) */
-        /* window.alert(87) */
-        /* console.log(57) */
+   /*  function myFunction(){
+        document.getElementById("demo").innerHTML="Hello Javascript" 
+        document.write(59) 
+        window.alert(87) 
+        console.log(57) 
         window.print()
-    }
+    } */
+const Y = 78;
+var X = 6;
+var X = 80;
+console.log(X)
